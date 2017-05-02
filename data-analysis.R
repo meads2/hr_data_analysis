9ddfe8e8e971726b9845211e48457f4ed9bdc0a5
Matt Eads <matteads2@gmail.com>
2017-05-01 20:41:18 -0400

    Updated Analysis

--- data-analysis.R
+++ data-analysis.R
@@ -3,10 +3,9 @@
 # ----------------------------------
 library(ggplot2)
 library(caret)
-library(tidyr)
 library(dplyr)
 library(cluster)
-library(ggcorrplot)
+library(pvclust)
 library(Amelia)
 library(rpart)
 library(MASS)
@@ -17,46 +16,73 @@ library(randomForest)
 # Set random seed
 set.seed(100)
 
-# ----------------------------------
-#        Load Data & Randomize
-# ----------------------------------
+# Load Data & Randomize
+
 # Full hr.data set
 hr.data <- read.csv('hr_data.csv', header = T)
-# Smaller set of data & minimize any systematic biases
+
+# Smaller set of data & minimize any system biases
 hr.data <- sample_n(hr.data, 4000)
 
-# ----------------------------------
-#       View The Types Of Data
-# ----------------------------------
-hr.data.types <- glimpse(hr.data) 
+# View The Types Of Data
+glimpse(hr.data) 
 
-# ----------------------------------
-#      Visualize Distributions
-# ----------------------------------
-# Only Interested In Certain Columns
+#########################################
+##                                     ##
+##          Data Exploration           ##
+##                                     ##
+#########################################
 
+# Employee Status Chart
+ggplot(hr.data, aes(x = left)) + 
+       geom_histogram(bins = 2, binwidth = .5, fill = 'orange') + 
+       labs(title = 'Employment Status Of HR Data', x = 'Status', y = 'Count')
 
-# ----------------------------------
-#      Calculate Summary Stats.
-# ----------------------------------
+# Distrbution By Employee Dept
+ggplot(hr.data, aes(x = sales)) +
+  geom_histogram(bins = 10,stat = 'count', binwidth = .5) +
+  theme_minimal(base_size = 16, base_family = 'Roboto') + 
+  geom_bar(aes(fill = sales)) + 
+  theme(axis.text.x = element_text(size=9, angle=45)) +
+  labs(title = 'Employee Count By Department', x = 'Department', y = 'Count')
+
+# Distribution Of Satisfaction Levels
+hist(hr.data$satisfaction_level,
+     breaks = 10,
+     col = 'red', 
+     main = 'Distribution Of Satisfaction Level', 
+     xlab = 'Satisfaction Level', 
+     ylab = 'Count'
+     )
+
+# Employee Leave By Projects
+barData <- table(as.factor(hr.data$left), hr.data$number_project)
+barplot(barData, 
+        main="Employees Left Vs. Projects",
+        xlab="No. of Projects", 
+        col=c("blue","orange"),
+        legend = rownames(barData), 
+        beside=T)
+
+# Calculate Summary Stats
 summary(hr.data)
 
-# ----------------------------------
-#      Visualize Missing Values
-# ----------------------------------
+# Visualize Missing Values
 missmap(hr.data, 
-        col = c('red', 'white'),
+        col = c('orange', 'white'),
         x.cex = .8,
         ylab = 'Row No.',
         xlab = 'Variable',
         y.cex = .3,
         main = 'Missing Data Of HR Data',
-        rank.order = T
+        rank.order = T,
+        y.labels = c(seq(1, 4000, by = 500)), 
+        y.at = c(seq(1, 4000, by = 500))
       )
 
 #########################################
 ##                                     ##
-##         1.Logistic Regression       ##
+##        1.Logistic Regression        ##
 ##                                     ##
 #########################################
 # Convert Factors Into Dummy Variables
@@ -65,77 +91,83 @@ salary.dummies <- dummy(hr.data$salary, sep = '_')
 hr.data.cleaned <- cbind(hr.data, sales.dummies, salary.dummies)
 hr.data.cleaned <- hr.data.cleaned[,c(1:8,11:23)]
 
-# ----------------------------------
-#   Create Training & Test Splits
-# ----------------------------------
+# Create Training & Test Splits
 # Create an 80/20 split
 split <- round(nrow(hr.data.cleaned) * .80)
 hr.train <- hr.data.cleaned[1:split, ]
 hr.test <- hr.data.cleaned[(split + 1):nrow(hr.data.cleaned), ]
 
-# ----------------------------------
-#  Run Benchmark Logistic Regresion
-# ----------------------------------
-# Logistic Model to predict whether an employee has left
-# FOR THIS LOGISTIC MODEL DO I HAVE TO WRAP THE RESPONSE
-# VARIABLE "LEFT" IN AS.FACTOR? FOR THE LOGISTIC REGRESSION
-# TO WORK PROPERLY?
+# Run Benchmark Logistic Regresion
+# Logistic Model to predict whether an employee leaves
 hr.model <- glm(left ~ ., family = 'binomial', hr.train)
+
+# Summary Of Model
 summary(hr.model)
+
+# Find the best predictors
+confint(hr.model, level = .95) 
+
+# Model Predictions
 hr.pred <- predict(hr.model, hr.test, type = 'response')
 
-# ----------------------------------
-#    Find Significant Cutoff Point
-# ----------------------------------
+# Find Significant Cutoff Point
 # Create a ROC Curve To Find Cutoff Point
-colAUC(hr.pred, hr.test$left, plotROC = T)
+# sensitivity vs. speceficity
 model.AUC <- colAUC(hr.pred, hr.test$left, plotROC = T)
 abline(h=model.AUC, col = 'red')
 text(.2,.9,cex = .8, labels = paste('Optimal Cutoff: ', round(model.AUC, 4)))
 
-# ----------------------------------
-#   Convert Probabilities To Class
-# ----------------------------------
+# Convert Probabilities To Class
 # 1 indicates the employee left,
 # 0 indicates the employee stayed
-
 # Covert model pred probabilities into classes
 p.class <- ifelse(hr.pred > .7860, 1, 0)
 
 # Create a confusion matrix
-# QUESTION: HOW TO CHANGE THE MATRIX TO SHOW 
-# 1 AS POSITIVE?
 confusionMatrix(p.class, hr.test$left)
-
 
 #########################################
 ##                                     ##
 ##         2.Cluster Analysis          ##
 ##                                     ##
 #########################################
-hr.clusters <- hclust(dist(hr.data[,1:8]))
-plot(hr.clusters)
-
-# Cluster Cut
-hr.cluster.cut <- cutree(hr.clusters, 10)
-cluster.counts <- table(hr.cluster.cut)
-plot(cluster.counts)
-
+hr.clusters <- pvclust(hr.data[,1:8])
+plot(hr.clusters, main = 'HR Data Cluster')
 
 #########################################
 ##                                     ##
-##            3. Anova Test            ##
+##            3. Anova Tests           ##
 ##                                     ##
 #########################################
-boxplot(hr.data)
-# Tukey's T-Test !!!!
-# Check All Assumptions !!!!
 
+# ANOVA: Time_Spend_Company By Sales
+anova <- aov(time_spend_company ~ sales, data = hr.data)
+summary(anova)
+TukeyHSD(anova, conf.level = .95)
+plot(time_spend_company ~ sales, 
+     data = hr.data, 
+     main = 'ANOVA: Time Spent At Company By Dept.',
+     xlab = '',
+     ylab = 'Time Spent At Company', 
+     las = 2,
+     cex = .6
+     )
 
+# ANOVA SATISFACTION LEVEL BY SALARY
+anova2 <- aov(satisfaction_level ~ salary, data = hr.data)
+summary(anova2)
+TukeyHSD(anova2)
+plot(satisfaction_level ~ salary, 
+     data = hr.data, 
+     col = c('orange', 'red', 'green'),
+     main = 'ANOVA: Satisfaction Level By Salary',
+     xlab = 'Salary Level',
+     ylab = 'Satisfaction Level'
+     )
 
 #########################################
 ##                                     ##
-##          4. Random Forest           ##
+##           4. Random Forest          ##
 ##                                     ##
 #########################################
 # Get A Fresh Copy Of The hr.data
@@ -155,4 +187,3 @@ summary(rf.pred)
 
 # View Confusion Matrix
 confusionMatrix(rf.pred, rf.test$left)
-
